05ec6bb1d6a3511f48ea358d39e82aa7a237ff53
HebaAlhamaydh <Eng_heba_hamaydh@hotmail.com>
2022-06-28 08:52:21 +0300

    do

--- modular/pilot/pilot.js
+++ modular/pilot/pilot.js
@@ -3,28 +3,28 @@
 require('dotenv').config();
 const io = require('socket.io-client');
 
+//name space
 let host = `http://localhost:${process.env.PORT}/airline`;
-let host2 = `http://localhost:${process.env.PORT}/`;
 const nameSpaceConnection = io.connect(host);
+
+nameSpaceConnection.on('new-flight', newFlight);
+function newFlight(Flight){
+setTimeout(() => {
+    Flight.event='took-off';
+    console.log(`Pilot: flight with ID ${ Flight.Details.flightID} took-off`) ;
+    nameSpaceConnection.emit('took-off', Flight);
+}, 4000)
+}
+let host2 = `http://localhost:${process.env.PORT}/`;
 const systemConnection2 = io.connect(host2);
 
-
-// nameSpaceConnection.on('new-flight', newFlight);
-// function newFlight(Flight){
-// setTimeout(() => {
-//     Flight.event='took-off';
-//     console.log(`Pilot: flight with ID ${ Flight.Details.flightID} took-off`) ;
-//     nameSpaceConnection.emit('took-off', Flight);
-// }, 4000)
-// }
-
-systemConnection2.on('new-flight', newFlight);
-function newFlight(Flight){
-    setTimeout(() => {
-            Flight.event='took-off';
-            console.log(`Pilot: flight with ID ${ Flight.Details.flightID} took-off`) ;
-            systemConnection2.emit('took-off', Flight);
-        }, 4000)
+systemConnection2.on('new-flight', newFlight2);
+function newFlight2(Flight){
+    // setTimeout(() => {
+    //         Flight.event='took-off';
+    //         console.log(`Pilot: flight with ID ${ Flight.Details.flightID} took-off`) ;
+    //         systemConnection2.emit('took-off', Flight);
+    //     }, 4000)
 
 setTimeout(() => {
     Flight.event='arrived';
