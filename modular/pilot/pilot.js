3959306948e04dadc9550466688bea0ed394b036
HebaAlhamaydh <Eng_heba_hamaydh@hotmail.com>
2022-06-29 10:44:15 +0300

    messageQueue

--- modular/pilot/pilot.js
+++ modular/pilot/pilot.js
@@ -3,11 +3,12 @@
 require('dotenv').config();
 const io = require('socket.io-client');
 
-//name space
+///////////////airline name space//////////////
 let host = `http://localhost:${process.env.PORT}/airline`;
 const nameSpaceConnection = io.connect(host);
 
 nameSpaceConnection.on('new-flight', newFlight);
+
 function newFlight(Flight){
 setTimeout(() => {
     Flight.event='took-off';
@@ -15,19 +16,25 @@ setTimeout(() => {
     nameSpaceConnection.emit('took-off', Flight);
 }, 4000)
 }
+/////////////base server/////////
 let host2 = `http://localhost:${process.env.PORT}/`;
 const systemConnection2 = io.connect(host2);
 
 systemConnection2.on('new-flight', newFlight2);
-function newFlight2(Flight){
-    // setTimeout(() => {
-    //         Flight.event='took-off';
-    //         console.log(`Pilot: flight with ID ${ Flight.Details.flightID} took-off`) ;
-    //         systemConnection2.emit('took-off', Flight);
-    //     }, 4000)
-
+function newFlight2(Flight){  
 setTimeout(() => {
     Flight.event='arrived';
     console.log(`Pilot: flight with ID ${ Flight.Details.flightID} has arrived`) ;
     systemConnection2.emit('arrived', Flight);
+    systemConnection2.emit('delete',Flight.Details.flightID);  
 }, 7000)}
+
+systemConnection2.emit('get_all');
+
+systemConnection2.on('flight',handleFlight);
+function handleFlight(flight){
+    
+    console.log(`Pilot:Sorry i didn't catch this flight ID ${flight.id} `);
+      systemConnection2.emit('delete',flight);
+}
+
--- modular/system/system.js
+++ modular/system/system.js
@@ -1,10 +1,17 @@
 'use strict';
 require('dotenv').config();
-
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
+// const uuid = require('uuid').v4;
 
 const ioServer  = require('socket.io')(PORT);
-//name space
+///add new object///
+const queue = {
+  flights: {
+
+  }
+}
+
+////////airline name space/////////
 const flightConnection = ioServer.of('/airline');
 
 flightConnection.on('connection', (socket) => {
@@ -13,23 +20,44 @@ flightConnection.on('connection', (socket) => {
       console.log(payload) ; 
   }
 })
-
+/////////////ioServer////////////
 ioServer.on('connection', (socket) => {
   socket.on('new-flight', newFlight);
   function newFlight(payload){
-  console.log(payload) ;
-  flightConnection.emit('new-flight',payload);
-  ioServer.emit('new-flight',payload);
+
+     const id = payload.Details.flightID;
+    //add payload to object
+    queue.flights[id] = payload;
+    console.log(payload) ;
+    
+    flightConnection.emit('new-flight',payload);
+    ioServer.emit('new-flight',payload);
   }
  
-  
-
   socket.on('arrived', flightArrived);
   function flightArrived(payload){
   console.log(payload) ; 
   ioServer.emit('arrived',payload)
   
   }
+
+  socket.on('get_all', () => {
+    // ['32ennn32323223ijij','42r545ggt66tve4646']
+   
+    Object.keys(queue.flights).forEach((id) => {
+        socket.emit('flight', {
+            id: id,
+            payload: queue.flights[id]
+        });
+        // console.log("*******content of queue*****",queue.flights) ;
+    })
+  })
+
+  socket.on('delete', (flight) => {
+    //this will remove the flight from the queue
+    delete queue.flights[flight.id];
+  })
+  
 
 });
 
